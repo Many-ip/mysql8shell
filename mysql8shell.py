178bc4908e1bd77ce744a937e51fa1d0af84ab95
Manel <57791064+Many-ip@users.noreply.github.com>
2020-12-06 14:09:14 +0100

    Update mysql8shell.py

--- mysql8shell.py
+++ mysql8shell.py
@@ -41,10 +41,49 @@ sql_res = my_session.sql(CREATE_TBL).execute()
 my_tbl = my_db.get_table('SuperPY')
 
 # Insert some rows (data)
-my_tbl.insert(COLUMNS).values('paint_vat_temp', 32.815, 'Celsius').execute()
-my_tbl.insert(COLUMNS).values('tongue_height_variance', 1.52, 'mm').execute()
-my_tbl.insert(COLUMNS).values('ambient_temperature', 24.5, 'Celsius').execute()
-my_tbl.insert(COLUMNS).values('gross_weight', 1241.01, 'pounds').execute()
+my_tbl.insert(COLUMNS).values('42345678A','caixer1','cognom1','987654321').execute()
+my_tbl.insert(COLUMNS).values('43345678B','caixer2','cognom2','977654322').execute()
+my_tbl.insert(COLUMNS).values('44345678C','caixer3','cognom3','967654323').execute()
+my_tbl.insert(COLUMNS).values('45345678D','caixer4','cognom4','957654324').execute()
 # Execute a simple select (SELECT ∗ FROM)
 print("\nShowing results after inserting all rows.")
 my_res = my_tbl.select(COLUMNS).execute()
+
+# Display the results . Demonstrates how to work with results
+# Print the column names followed by the rows
+column_names = my_res.get_column_names()
+column_count = my_res.get_column_count()
+
+for i in range(0,column_count):
+    if i < column_count - 1:
+        print "{0}, ".format(column_names[i]),
+    else:
+        print "{0}".format(column_names[i]),
+print
+for row in my_res.fetch_all():
+    for i in range(0,column_count):
+        if i < column_count - 1:
+            print "{0}, ".format(row[i]),
+        else:
+            print "{0}".format(row[i]),
+    print
+
+# Update a row
+my_tbl.update().set('Cognom_Caixer', 'Update1').where('Id_Caixer LIKE 1').execute()
+my_res = my_tbl.select(COLUMNS).execute()
+
+# Display the results
+for row in my_res.fetch_all():
+    print row
+    
+    
+# Delete some rows
+my_tbl.delete().where("Id_Caixer LIKE 3").execute()
+
+# Execute a simple select (SELECT ∗ FROM)
+print("\nShowing results after deleting rows with nombre = Lorem ")
+my_res = my_tbl.select(COLUMNS).execute()
+# Display the results
+for row in my_res.fetch_all():
+    print row
+
